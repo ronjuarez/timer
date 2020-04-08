7cc8dc6b91f41c802ca4c4c79f880c4eb1a693b5
Ron Juarez <ronstewartjuarez@gmail.com>
2020-04-07 21:19:34 -0400

    added two conditions to the function to prevent it from failing edge cases

--- timer1.js
+++ timer1.js
@@ -1,7 +1,10 @@
 const timerLengths = process.argv.slice(2);
 
+if (timerLengths){
   for (const time of timerLengths) {
-    setTimeout(() => {
-      process.stdout.write('\x07');
-}, 1000 * time)
+    if (time > 0){
+      setTimeout(() => {process.stdout.write('\x07');
+      }, 1000 * time);
+    }
   }
+};
